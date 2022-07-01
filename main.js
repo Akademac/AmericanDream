fe10a4ea15365ff4a4d6996cdcbb9adfa160be9f
Djuki <gdjuric1989@gmail.com>
2022-07-01 20:12:37 +0200

    fourth commit

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
+//Mini Nav
+
+let burger = document.querySelector('.burgerIcon');
+let mini_nav = document.querySelector('.mini__navDiv ');
+let nav_x = false;
+
+burger.addEventListener('click', () => {
+  if(!nav_x) {
+    mini_nav.style.width = '100%';
+    nav_x = true
+  }
+  else {
+    mini_nav.style.width = '0%';
+    nav_x = false;
+  }
+})
+
+mini_nav.addEventListener('click', () => {
+  mini_nav.style.width = '0%';
+  nav_x = false;
+})
+
 // Go to second page
 
 let goToSecondPage = document.querySelectorAll('.goToSecondPage');
@@ -7,3 +29,4 @@ goToSecondPage.forEach( e => {
     window.open('http://127.0.0.1:5500/registration.html', '_self');
   })
 })
+
