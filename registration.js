0bf6165bbab69bd321f55da9d6e129ff3c30feb7
Djuki <gdjuric1989@gmail.com>
2022-07-02 20:48:50 +0200

    details #1

--- registration.js
+++ registration.js
@@ -6,3 +6,17 @@ backBtn.addEventListener('click', () => {
   // window.open('http://127.0.0.1:5500/index.html', '_self');
   window.open('https://akademac.github.io/AmericanDream/index.html', '_self')
 })
+
+// Social Media
+
+let aD_instaIcon = document.querySelector('.aD_instaIcon');
+let aD_faceIcon = document.querySelector('.aD_faceIcon');
+
+aD_instaIcon.addEventListener('click', () => {
+  window.open('https://www.instagram.com/americandream.work/?igshid=YmMyMTA2M2Y=&fbclid=IwAR0Slnrj5sHLnOtFvkPY1s_ZyQp-3u0ZaR-kSRx4PDSVHhs6AarGyOdpABs');
+})
+
+aD_faceIcon.addEventListener('click', () => {
+  window.open('https://m.facebook.com/americandream.work/');
+})
+
